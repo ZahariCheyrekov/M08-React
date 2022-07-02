410c937f0b9d8599ffe955acba68bb37f5a31a8b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-02 19:51:56 +0300

    Add Footer.js

---
+++ L01_IntroToReactAndJSX/exercise/src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p className="footer-content">&copy; 2017-2020 SoftUni Creative. All Rights Reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
