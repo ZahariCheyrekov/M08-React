feae51249c554618f15cf5f0114553f347d9ad43
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-02 23:58:32 +0300

    Add List.js

---
+++ L02_ComponentsBasicIdea/demo/src/components/List.js
@@ -0,0 +1,12 @@
+import ListItem from "./ListItem";
+
+function List() {
+    return (
+        <ul>
+            <ListItem text="Demo item text" />
+            <ListItem text="Demo second li item" />
+        </ul >
+    );
+}
+
+export default List;
