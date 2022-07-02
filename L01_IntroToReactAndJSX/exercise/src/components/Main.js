47efde4f40d7f2237b29c5a28af2ac5f3f916459
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-02 19:51:48 +0300

    Add Main.js

---
+++ L01_IntroToReactAndJSX/exercise/src/components/Main.js
@@ -0,0 +1,17 @@
+import Benefits from "./Benefits";
+import Taxes from "./Taxes";
+import Training from "./Training";
+
+function Main() {
+    return (
+        <main className="main-container" >
+            <Taxes />
+
+            <Training />
+
+            <Benefits />
+        </main >
+    );
+}
+
+export default Main;
