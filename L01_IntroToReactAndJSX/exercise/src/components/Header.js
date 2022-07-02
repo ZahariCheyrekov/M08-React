326e31362576f0b0d7aeaa63da1c593ffbb4ebbb
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-02 19:52:44 +0300

    Add Header.js

---
+++ L01_IntroToReactAndJSX/exercise/src/components/Header.js
@@ -0,0 +1,24 @@
+function Header() {
+    return (
+        <header className="header-page">
+            <nav className="header-nav">
+                <ul className="header-nav-list">
+                    <li className="nav-li-item">
+                        <a href="#">ЗА НАС</a>
+                    </li>
+                    <li className="nav-li-item">
+                        <a href="#">ФОРУМ</a>
+                    </li>
+                    <li className="nav-li-item">
+                        <a href="#">БЛОГ</a>
+                    </li>
+                    <li className="nav-li-item">
+                        <a href="#">КОНТАКТИ</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
