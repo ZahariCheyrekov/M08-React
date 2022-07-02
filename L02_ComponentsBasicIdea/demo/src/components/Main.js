8b9e6d299097bc3e2b0e5b660bbeda1a11ca1932
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-02 23:58:48 +0300

    Add Main.js

---
+++ L02_ComponentsBasicIdea/demo/src/components/Main.js
@@ -0,0 +1,12 @@
+import Counter from "./Counter";
+import List from "./List";
+
+function Main() {
+    return (
+        <main>
+            <List />
+            <Counter />
+        </main>
+    );
+}
+export default Main;
