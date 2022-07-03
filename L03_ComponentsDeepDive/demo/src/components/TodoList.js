bb349df00828da93e258b000959d4f76935f3201
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-03 12:26:13 +0300

    Add TodoList.js

---
+++ L03_ComponentsDeepDive/demo/src/components/TodoList.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import uniqid from 'uniqid';
+import TodoListItem from './TodoListItem';
+
+export default function TodoList() {
+    const [todos, setTodos] = React.useState([
+        { id: uniqid(), text: 'Clean my room' },
+        { id: uniqid(), text: 'Make breakfast' },
+        { id: uniqid(), text: 'Wash the dishesh' },
+    ]);
+
+    const onToDoBlur = (ev) => {
+        let todo = {
+            id: uniqid(),
+            text: ev.target.value
+        };
+
+        setTodos(oldTodos => [
+            ...oldTodos,
+            todo
+        ]);
+    }
+
+    const onDeleteTodoItemHanlder = (id) => {
+        setTodos(oldTodos => oldTodos.filter(todo => todo.id !== id));
+    }
+
+    return (
+        <>
+            <label htmlFor="todo-name">Add Todo</label>
+            <input type="text" id="todo-id" onBlur={onToDoBlur} name="todo" />
+            <ul>
+                {todos.map(todo => (
+                    <TodoListItem
+                        key={todo.id}
+                        id={todo.id}
+                        text={todo.text}
+                        onDelete={onDeleteTodoItemHanlder}
+                    />
+                ))}
+            </ul>
+        </>
+    );
+}
