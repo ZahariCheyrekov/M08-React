28b2623b44fdd599e8e23e61094c8845eedda2c9
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-03 22:21:47 +0300

    Add router in main element

--- L05_Routing/demo/src/components/Main.js
+++ L05_Routing/demo/src/components/Main.js
@@ -6,22 +6,19 @@ import EditGame from './EditGame';
 import GameDetails from './GameDetails';
 import GameCatalog from './GameCatalog';
 
+import { Route, Switch } from 'react-router-dom';
+
 const Main = () => {
     return (
         <main id="main-content">
-            <Home />
-
-            <Login />
-
-            <Register />
-
-            <CreateGame />
-
-            <EditGame />
-
-            <GameDetails />
-
-            <GameCatalog />
+            <Switch>
+                <Route path={"/"} exact component={Home} />
+                <Route path={"/login"} component={Login} />
+                <Route path={"/register"} component={Register} />
+                <Route path={"/edit"} component={EditGame} />
+                <Route path={"/catalog"} component={GameCatalog} />
+                <Route path={"/create"} component={CreateGame} />
+            </Switch>
         </main>
     );
 }
