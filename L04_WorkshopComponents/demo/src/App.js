595c87bb66c5096c71bd01af83a7cf195b13cb32
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-03 17:09:28 +0300

    Exctact html from app

--- L04_WorkshopComponents/demo/src/App.js
+++ L04_WorkshopComponents/demo/src/App.js
@@ -1,9 +1,13 @@
-function App() {
-  return (
-    <div >
+import Header from './components/Header';
+import Main from './components/Main';
 
-    </div>
-  );
+function App() {
+    return (
+        <div id="box">
+            <Header />
+            <Main />
+        </div>
+    );
 }
 
 export default App;
