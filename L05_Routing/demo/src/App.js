4dcb678772e9275b2dad24454fcd532eae5eb065
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-03 22:22:22 +0300

    Add app

--- L05_Routing/demo/src/App.js
+++ L05_Routing/demo/src/App.js
@@ -2,12 +2,12 @@ import Header from './components/Header';
 import Main from './components/Main';
 
 function App() {
-    return (
-        <div id="box">
-            <Header />
-            <Main />
-        </div>
-    );
+  return (
+    <div id="box">
+      <Header />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
