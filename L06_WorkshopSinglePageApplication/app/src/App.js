94d52e4e704abdd4e4802ccbbd837f7b9a531127
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-04 12:28:19 +0300

    Add user authentication logic

--- L06_WorkshopSinglePageApplication/app/src/App.js
+++ L06_WorkshopSinglePageApplication/app/src/App.js
@@ -7,16 +7,37 @@ import MyPets from './components/MyPets';
 import Footer from './components/Footer';
 
 import { Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import * as userService from './services/userService';
 
 function App() {
+   const [user, setUser] = useState({ isAuthenticated: false, user: null });
+
+   useEffect(() => {
+      const localUser = userService.getUser();
+
+      setUser({
+         isAuthenticated: Boolean(localUser),
+         user
+      });
+   }, []);
+
+   const onLogin = (username) => {
+      setUser({
+         isAuthenticated: true,
+         user: username
+      });
+   }
+
    return (
       <div id="container">
-         <Header />
+         <Header {...user} />
 
          <main id="site-content">
             <Routes>
                <Route path='/' element={<Dashboard />} />
-               <Route path='/login' element={<Login />} />
+               <Route path='/login' element={<Login onLogin={onLogin} />} />
                <Route path='/register' element={<Register />} />
                <Route path='/create' element={<Create />} />
                <Route path='/my-pets' element={<MyPets />} />
