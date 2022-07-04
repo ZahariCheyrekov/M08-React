fce4820b83ddc91392bdda0f2b44e541f303bbc7
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-04 17:41:20 +0300

    Add login submit handler

--- L08_Forms/demo/src/App.js
+++ L08_Forms/demo/src/App.js
@@ -1,7 +1,39 @@
 function App() {
+  const loginSubmitHandler = (ev) => {
+    ev.preventDefault();
+
+    const { email, password } = Object.fromEntries(new FormData(ev.currentTarget));
+
+    const user = {
+      email,
+      password
+    }
+
+    localStorage.setItem('user', JSON.stringify(user));
+
+    ev.target.reset();
+  }
+
   return (
     <div>
-
+      <form onSubmit={loginSubmitHandler} id="login-form" action="" method="POST">
+        <fieldset>
+          <legend>Login Form</legend>
+          <p className="field">
+            <label htmlFor="email">Email</label>
+            <span className="input">
+              <input type="text" name="email" id="email" placeholder="Email" />
+            </span>
+          </p>
+          <p className="field">
+            <label htmlFor="password">Password</label>
+            <span className="input">
+              <input type="password" name="password" id="password" placeholder="Password" />
+            </span>
+          </p>
+          <input className="button submit" type="submit" defaultValue="Login" />
+        </fieldset>
+      </form>
     </div>
   );
 }
