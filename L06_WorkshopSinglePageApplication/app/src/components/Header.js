2025cc7de1615d55953e88b43caa86dca00f5ae9
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-04 12:27:00 +0300

    Add header navigation links

--- L06_WorkshopSinglePageApplication/app/src/components/Header.js
+++ L06_WorkshopSinglePageApplication/app/src/components/Header.js
@@ -1,22 +1,32 @@
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ isAuthenticated, user }) => {
+    const userLinks = () => {
+        return (
+            <div id="user">
+                <span>Welcome, {user}</span>
+                <Link className="button" to="/my-pets">My Pets</Link>
+                <Link className="button" to="/create">Add Pet</Link>
+                <Link className="button" to="/logout">Logout</Link>
+            </div>
+        );
+    }
+    
+    const guestLinks = () => {
+        return (
+            <div id="guest">
+                <Link className="button" to="/login">Login</Link>
+                <Link className="button" to="/register">Register</Link>
+            </div>
+        );
+    }
+
     return (
         <header id="site-header">
             <nav className="navbar">
                 <section className="navbar-dashboard">
                     <Link to="/">Dashboard</Link>
-                    <div id="guest">
-                        <Link className="button" to="/login">Login</Link>
-                        <Link className="button" to="/register">Register</Link>
-                    </div>
-
-                    <div id="user">
-                        <span>Welcome, email add</span>
-                        <Link className="button" to="/my-pets">My Pets</Link>
-                        <Link className="button" to="/create">Add Pet</Link>
-                        <Link className="button" to="/logout">Logout</Link>
-                    </div>
+                    {isAuthenticated ? userLinks() : guestLinks()}
                 </section>
             </nav>
         </header>
