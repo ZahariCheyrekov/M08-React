d6707359ade17b38c95dbe961de12faf05f0ec1b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-05 11:16:47 +0300

    Add unique list key to list items

--- L08_Hooks/exercise/src/components/CharacterList.js
+++ L08_Hooks/exercise/src/components/CharacterList.js
@@ -1,11 +1,12 @@
 import uniqid from 'uniqid';
+import useFetch from '../hooks/useFetch';
 
 const CharacterList = () => {
     const characters = useFetch('https://swapi.dev/api/people');
 
     return (
         <ul>
-            {characters.map(x => <li key={uniqid}>{x}</li>)}
+            {characters.map(x => <li key={uniqid()}>{x.name}</li>)}
         </ul>
     );
 }
