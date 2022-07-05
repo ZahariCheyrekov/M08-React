1d109e5cffd8dd8be9d4e3302b873fc4aaf73305
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-05 10:06:21 +0300

    Add setInfo function

--- L08_Hooks/exercise/src/App.js
+++ L08_Hooks/exercise/src/App.js
@@ -1,25 +1,36 @@
 import Counter from './components/Counter';
 import { useState, useEffect } from 'react';
+import uniqid from 'uniqid';
 
 function App() {
-  const [name, setName] = useState('');
-  const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
+    const [count, setCount] = useState(0);
+    const [info, setInfo] = useState({
+        name: 'Atlas',
+        age: 1000,
+        hobbies: ['studying', 'exercising', 'reading']
+    });
 
-  useEffect(() => {
+    useEffect(() => {
+        setTimeout(() => {
+            setName('Atlas');
 
-    setTimeout(() => {
-      setName('Atlas');
-    }, 1000);
+            setInfo(oldState => ({
+                ...oldState,
+                hobbies: [...oldState.hobbies, 'running']
+            }));
+        }, 1000);
+    }, [count]);
 
-  }, []);
-
-  return (
-    <div>
-      <h2>{!name ? 'Loading...' : name}</h2>
-      <Counter count={count} />
-      <button onClick={() => setCount(count + 1)}>+</button>
-    </div>
-  );
+    return (
+        <div>
+            <h2>{!name ? 'Loading...' : name}</h2>
+            <Counter count={count} />
+            {info.hobbies.map(h => <li key={uniqid()}>{h}</li>)
+            }
+            <button onClick={() => setCount(count + 1)}>+</button>
+        </div >
+    );
 }
 
 export default App;
