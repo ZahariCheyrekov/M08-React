a937bbdd48841c13bf772d92b1aa40454251e897
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-07-05 09:43:10 +0300

    Add useState declaration

--- L08_Hooks/exercise/src/App.js
+++ L08_Hooks/exercise/src/App.js
@@ -1,7 +1,17 @@
+import Counter from './components/Counter';
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [name, setName] = useState('');
+  const [count, setCount] = useState(0);
+
+ 
+
   return (
     <div>
-
+      <h2>{!name ? 'Loading...' : name}</h2>
+      <Counter count={count} />
+      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
 }
